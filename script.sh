dfc707efaac84dba77211b50e4ac827a6c66b380
Muhammad Talha Tahir <talha.tahir@systemsltd.com>
2023-12-29 06:08:49 +0500

    test

--- script.sh
+++ script.sh
@@ -11,8 +11,8 @@ git checkout $DEV_BRANCH
 # Fetch the latest changes from PROD_BRANCH
 git fetch origin $PROD_BRANCH
 
-# Merge changes from PROD_BRANCH into DEV_BRANCH, with automatic conflict resolution favoring DEV_BRANCH
-git merge --no-commit --no-ff -Xours origin/$PROD_BRANCH
+# Merge changes from PROD_BRANCH into DEV_BRANCH, but do not commit yet
+git merge --no-commit --no-ff origin/$PROD_BRANCH
 
 # Check if there are changes in the EXCLUDE_DIR
 if git diff --cached --name-only | grep -q "$EXCLUDE_DIR"
@@ -20,12 +20,16 @@ then
     # Reset the EXCLUDE_DIR to the current state of DEV_BRANCH
     git reset HEAD $EXCLUDE_DIR
     git checkout -- $EXCLUDE_DIR
+fi
 
-    # Add a message indicating the directory has been excluded
-    echo "Excluded changes to $EXCLUDE_DIR"
+# If there are conflicts, resolve them by checking out the version from PROD_BRANCH
+if git ls-files -u | cut -f 2 | uniq
+then
+    git ls-files -u | cut -f 2 | uniq | grep -v "$EXCLUDE_DIR" | xargs -I{} git checkout --theirs {}
+    git add -u
 fi
 
 # Commit the merge
-git commit -m "Merged $PROD_BRANCH into $DEV_BRANCH excluding $EXCLUDE_DIR with automatic conflict resolution"
+git commit -m "Merged $PROD_BRANCH into $DEV_BRANCH with automatic conflict resolution"
 
 echo "Merge completed successfully."
